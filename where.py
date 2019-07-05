817d8dd5ed1d9369820dffa74b7f030f66a7dd63
lsp0 <liamsaliba@hotmail.com>
2019-07-06 01:07:02 +1000

    it's alive!

--- where.py
+++ where.py
@@ -3,7 +3,7 @@ Location sharing lib wrapper
 Liam Saliba
 2018-06-24, 2019-01-29, 2019-02-02
 """
-from locationsharinglib import Service
+from locationsharinglib import Service, InvalidCookies
 from geopy.geocoders import Nominatim
 from geopy import distance
 import requests
@@ -13,9 +13,7 @@ import getpass
 import datetime
 import os
 from threading import Timer
-
-
-geolocator = Nominatim(user_agent="whereis-friend")
+import string
 
 def get_time_displacement(dt):
     delta = datetime.datetime.now(dt.tzinfo) - dt
@@ -41,6 +39,8 @@ YOU_THRESH = 50
 HOME_THRESH = 150
 FNAME = "save.dat"
 POLLING_DURATION = 30
+PRINT_UPDATES = False
+
 
 class Person:
     def __init__(self, person, you=None, home=None):
@@ -132,7 +132,7 @@ class Person:
                 elif self.distance > 10000:
                     d = "%d km" % (self.distance / 1000)
                 middle = "%s away, %s" % (d, middle)
-        return "%s is %s.  (%s)" % (self.nickname, middle, get_time_displacement(self.datetime))
+        return "%s is %s, as of %s." % (self.nickname, middle, get_time_displacement(self.datetime))
 
     def print(self):
         print(self.get_info_str())
@@ -155,24 +155,30 @@ class You(Person):
         if self.at_home:
             place += "home: "
         place += self.address
-        return "You are at %s.   (%s)" % (place, get_time_displacement(self.datetime))
+        return "You are at %s, as of %s." % (place, get_time_displacement(self.datetime))
 
     def __repr__(self):
         return "You ({}) {}".format(self.id, str(self))
 
 class FrontEnd:
-    def __init__(self, username, password):
-        self.service = Service(username, password)
+    def __init__(self, username, cookie):
+        try:
+            self.service = Service(cookie, username)
+        except InvalidCookies:
+            print("Invalid cookie.")
+            raise InvalidCookies
         print("Authenticated.\n")
+        print(list(self.service.get_all_people()))
         self.now = None
         self.you = None
         self.people = []
+        # dictionary of people by id
         self.person_d = {}
+        self.person_n = {}
         self.load()
-        self.update()
+        self.auto_update()
 
-        t = Timer(POLLING_DURATION, self.update)
-        t.start()
+        
 
     def load(self):
         if not os.path.isfile(FNAME):
@@ -186,21 +192,27 @@ class FrontEnd:
             self.person_d[pid] = home
         print("Loaded.")
 
+    def auto_update(self):
+        self.update()
+        # update every N seconds (on a thread)
+        self.t = Timer(POLLING_DURATION, self.auto_update)
+        self.t.start()
 
     def update(self):
         self.now = datetime.datetime.now()
-        print("Updating... (%s)\n" % self.now.strftime("%H:%M"))
+        if PRINT_UPDATES:
+            print("Updating... (%s)\n> " % self.now.strftime("%H:%M:%S"))
         you = self.service.get_authenticated_person()
 
-        if you.id in self.person_d.keys():
+        if self.person_d.get(you.id) is None:
+            self.you = You(you)
+        else:
             # both 'homes' and people stored in the person dictionary
             if type(self.person_d[you.id]) == tuple:
                 self.you = You(you, home=self.person_d[you.id])
             else:
                 self.you = self.person_d[you.id]
                 self.you.update(you)
-        else:
-            self.you = You(you)
         self.person_d[you.id] = self.you
 
         self.people = []
@@ -217,6 +229,7 @@ class FrontEnd:
                 new_p = Person(p, you=self.you)
             self.person_d[p.id] = new_p
             self.people.append(new_p)
+            self.person_n[new_p.nickname.lower()] = new_p
 
         # sort by furthest distance
         self.people.sort(key=lambda p: p.distance)
@@ -229,15 +242,73 @@ class FrontEnd:
             p.print()
         print("(Refreshed at %s.)" % self.now.strftime('%Y-%m-%d %H:%M:%S'))
 
-    def set_home(self, i):
-        if i > len(self.people):
-            return
-        if i == 0:
-            self.you.set_home()
+    def who_find(self):
+        if len(self.people):
+            return "I've got no-one to find."
+        res = "I can find "
+        lst = [str(p.nickname) for p in self.people]
+        if len(lst) == 1:
+            res += lst[0] + "."
         else:
-            i -= 1
-            self.people[i].set_home()
-        self.save()
+            res += ", ".join(lst[:-1]) + " and " + lst[-1] + "."
+
+    def who_home(self):
+        people_at_home = []     
+        for i, p in enumerate(self.people):
+            if p.at_home:
+                people_at_home.append(p)
+
+        if len(people_at_home) == 0:
+            if self.you.at_home:
+                return "You are at home by yourself."
+            else:
+                return "No-one is home."
+        else:
+            res = ""
+            if self.you.at_home:
+                res += "You are at home with "
+            lst = [str(p.nickname) for p in people_at_home]
+            if len(lst) == 1:
+                res += lst[0] + "."
+            else:
+                res += ", ".join(lst[:-1]) + " and " + lst[-1] + "."
+            return res
+
+    def who_out(self):
+        people_out = []     
+        for i, p in enumerate(self.people):
+            if not p.at_home:
+                people_out.append(p)
+
+        if len(people_out) == 0:
+            if self.you.at_home:
+                return "Everyone is home."
+            else:
+                return "Everyone but you is home."
+        else:
+            res = ""
+            if not self.you.at_home:
+                res += "You, "
+            lst = [str(p.nickname) for p in people_out]
+            if len(lst) == 1:
+                res += lst[0]
+            else:
+                res += ", ".join(lst[:-1]) + " and " + lst[-1]
+            return res + " are out."
+
+    def set_home(self, name):
+        if name == 'me':
+            self.you.set_home()
+            self.save()
+            return "Set your home to where you are."
+        else:
+            p = self.person_n.get(name.lower())
+            if p is None:
+                return "Could not find {}.".format(name)
+            else:
+                p.set_home()
+                self.save()
+                return "Set home of {}.".format(p.nickname)
 
     def save(self):
         f = open(FNAME, "w")
@@ -246,24 +317,90 @@ class FrontEnd:
             if x is not None:
                 f.write(x)
 
+    def whereis(self, name):
+        if name == 'me':
+            return self.you.get_info_str()
+        else:
+            p = self.person_n.get(name.lower())
+            if p is None:
+                return "Could not find where \"{}\" is.".format(name)
+            else:
+                return p.get_info_str()
+
+USERNAME = input("Enter email address: ")
+COOKIE = "location_sharing.cookies"
+
+print("whereis")
+
+fe = FrontEnd(USERNAME, COOKIE)
+
+from flask import Flask
+from flask_assistant import Assistant, ask, tell
+import logging
+logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
+
+app = Flask(__name__)
+assist = Assistant(app, route='/')
 
 
-print("whereis --- \nEnter Google account login...")
-username = input("Username: ")
-password = getpass.getpass("Password: ")
+@assist.action('who-home')
+def who_home():
+    return ask(fe.who_home())
 
-fe = FrontEnd(username, password)
+@assist.action('who-out')
+def who_out():
+    return ask(fe.who_out())
 
-while True:
-    fe.print_all()
-    i = input("\n> ")
-    if i == "x" or i == 'exit':
-        break
-    cmds = i.split()
-    if len(cmds) > 0 and (cmds[0] == "sethome" or cmds[0] == 's') and cmds[1].isnumeric():
-        fe.set_home(int(cmds[1]))
-    else:
-        fe.update()
-    print("")
+@assist.action('who-find')
+def who_find():
+    return ask(fe.who_find())
 
-print("Exiting...\n")
+@assist.action('where-all')
+def where_all():
+    return ask(fe.who_find())
+
+@assist.action('where-person')
+def where_person(any):
+    return ask(fe.whereis(any.translate(str.maketrans('', '', string.punctuation))))
+
+@assist.action('where-me')
+def where_person():
+    return ask(fe.whereis("me"))
+
+@assist.action('set-home')
+def set_home(p):
+    print("recieved: ", p)
+    return ask(fe.sethome(any.translate(str.maketrans('', '', string.punctuation))))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# fe.t.cancel()
+# if __name__ == '__main__':
+#     fe.print_all()
+
+#     while True:
+#         i = input("\n> ")
+#         if i == "x" or i == 'exit':
+#             break
+#         cmds = i.split()
+#         if len(cmds) == 0:
+#             fe.update()
+#             fe.print_all()
+#         else:
+#             if cmds[0] in ["sethome", 's']:
+#                 if len(cmds) == 1:
+#                     fe.set_home(0)
+#                 elif cmds[1].isnumeric():
+#                     fe.set_home(int(cmds[1]))
+#             if cmds[0] in ["home", 'h']:
+#                 fe.print_home()
+#             if cmds[0] in ['whereis', 'where', 'w']:
+#                 if len(cmds) == 1:
+#                     print("You didn't look for anyone.")
+#                 else:
+#                     fe.whereis(cmds[1])
+#             if cmds[0] in ['q', 'exit', 'quit']:
+#                 break
+#         print("")
+#     fe.t.cancel()
